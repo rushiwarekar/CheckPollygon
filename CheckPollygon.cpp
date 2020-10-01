96d116bf9e6ad7c8f00b7775e3ee6a2f6e5f756b
rushiwarekar <56586917+rushiwarekar@users.noreply.github.com>
2020-10-01 16:49:18 +0530

    Update CheckPollygon.cpp

--- CheckPollygon.cpp
+++ CheckPollygon.cpp
@@ -4,6 +4,7 @@
 #include<stdio.h>
 #include<iostream>
 using namespace std;
+
 int polygon(int numvert, float *vertexX, float *vertexY, float testX, float testY)
 {
   int i, j, c = 0;
@@ -16,24 +17,24 @@ int polygon(int numvert, float *vertexX, float *vertexY, float testX, float test
 }
 int main(){
 	int t;
-	cout<<"Enter number of testcases:"<<endl;
-	cin>>t;
+		cout<<"Enter number of testcases:"<<endl;
+		cin>>t;
 	while(t--){
 		int n;
-		cout<<"Enter number of vertices"<<endl;
-		cin>>n;
+			cout<<"Enter number of vertices"<<endl;
+			cin>>n;
 		float x[n],y[n];
 		float px,py;
-		cout<<"Enter vertices using space separation:"<<endl;
+			cout<<"Enter vertices using space separation:"<<endl;
 		for(int i=0;i<n;i++){
 			cin>>x[i]>>y[i];
 		}
-		cout<<"Enter test point:"<<endl;
-		cin>>px>>py;
-		int c=polygon(n,x,y,px,py);
+			cout<<"Enter test point:"<<endl;
+			cin>>px>>py;
+			int c=polygon(n,x,y,px,py);
 		if(c==0)
-		cout<<"False"<<endl;
+			cout<<"False"<<endl;
 		else
-		cout<<"True"<<endl;
+			cout<<"True"<<endl;
 	}
 }
